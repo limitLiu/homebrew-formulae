61ece299d608ef43597ec24cc1d7317f283c851b
limit <limitliu@qq.com>
2024-10-07 19:27:54 +0800

    feat: update neovim v0.10.2

--- Casks/neovim.rb
+++ Casks/neovim.rb
@@ -1,16 +1,16 @@
 cask 'neovim' do
   architecture = Hardware::CPU.intel? ? :x86_64 : :arm64
-  version '0.10.1'
+  version '0.10.2'
 
   name 'Neovim Stable'
   homepage 'https://neovim.io/'
 
   case architecture
   when :x86_64
-    sha256 "dd88c86164e6fb34ee364c4a2b42c6a1832890003ae7c9c733032697d92cf7a6"
+    sha256 "8be3960d6ed843121a8d00af38d47ef6757824fc05a50a9bc240d678d61a5463"
     url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-macos-x86_64.tar.gz"
   when :arm64
-    sha256 "4b322a8da38f0bbdcdcc9a2b224a7b5267f0b1610b7345cb880d803e03bb860b"
+    sha256 "cd3e2e97fee285c41016cece51e67e8174df1dc577b89bc04063b2068ccdaea0"
     url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-macos-arm64.tar.gz"
   else
     raise "Unsupported architecture"
