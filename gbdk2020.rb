0523281fc0d23ee59d92b86c0141d77bd70ea7df
Limit Liu <limitliu@qq.com>
2025-05-07 12:36:49 +0800

    chore: gbdk2020 bump to v4.3.0

--- gbdk2020.rb
+++ gbdk2020.rb
@@ -1,8 +1,19 @@
 class Gbdk2020 < Formula
   desc "Game Boy Development Kit 2020"
   homepage "https://github.com/gbdk-2020/gbdk-2020"
-  url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.2.0/gbdk-macos.tar.gz"
-  sha256 "76d157cea3ee1ad5bbbb37ccd95afb7b8e8a4e60f6bdda778f98ff4ecc55e62f"
+
+  architecture = Hardware::CPU.intel? ? :x86_64 : :arm64
+
+  case architecture
+  when :x86_64
+    sha256 "c796089c03f64153cf67c49568416ce05ac1865d2eac59ae4c58445b974eb619"
+    url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.3.0/gbdk-macos.tar.gz"
+  when :arm64
+    sha256 "d8300880ef65e291b226db6c82e116f669aff294f1bea506c41729e8cc86a1f6"
+    url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.3.0/gbdk-macos-arm64.tar.gz"
+  else
+    raise "Unsupported architecture"
+  end
 
   def install
     bin.install Dir["bin/*"]
