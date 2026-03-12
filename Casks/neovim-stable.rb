65fb9300d48a11a6f7e79114dab76bba91140802
Limit Liu <limitliu@qq.com>
2026-03-12 19:51:23 +0800

    feat: update gbdk and neovim version

--- Casks/neovim-stable.rb
+++ Casks/neovim-stable.rb
@@ -1,16 +1,16 @@
-cask 'neovim' do
-  architecture = Hardware::CPU.intel? ? :x86_64 : :arm64
-  version '0.11.5'
+cask 'neovim-stable' do
+  version :latest
+  sha256 :no_check
 
-  name 'Neovim Stable'
+  name 'Neovim Nightly'
   homepage 'https://neovim.io/'
+
+  architecture = Hardware::CPU.intel? ? :x86_64 : :arm64
 
   case architecture
   when :x86_64
-    sha256 "6612760a7037ca2518e456908baf5e43101fa79819d18979fc4d4e8441d9dfa5"
     url "https://github.com/neovim/neovim/releases/download/stable/nvim-macos-x86_64.tar.gz"
   when :arm64
-    sha256 "79143d3b408f7034f90b7cf59af2276de09ef8a4c2f1a28e4c99581b249d3107"
     url "https://github.com/neovim/neovim/releases/download/stable/nvim-macos-arm64.tar.gz"
   else
     raise "Unsupported architecture"
--- gbdk2020.rb
+++ gbdk2020.rb
@@ -1,16 +1,16 @@
 class Gbdk2020 < Formula
   desc "Game Boy Development Kit 2020"
   homepage "https://github.com/gbdk-2020/gbdk-2020"
-  version '4.4.0'
+  version '4.5.0'
 
   architecture = Hardware::CPU.intel? ? :x86_64 : :arm64
 
   case architecture
   when :x86_64
-    sha256 "ba7ffc51c12fd1625fc99850691e0f5312cb6169691d39823c67972e685db9bc"
+    sha256 "1aa549d12032d8f6509d11923bb28b1a453098f42597feb378e9a42541f8fd89"
     url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-macos.tar.gz"
   when :arm64
-    sha256 "0c67b5cafff8a617729b77f9383ea41eb864b34f52c2391686c94427936db2df"
+    sha256 "289ee60e46c5a2785a21e35533f84a5131ed4a063b21b0dbdedc9a10af15bf78"
     url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-macos-arm64.tar.gz"
   else
     raise "Unsupported architecture"
